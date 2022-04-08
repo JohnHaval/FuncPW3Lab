a3b294c1ab35e12b16ecd2feba5abaf64c100d07
Hapro Bishop <acfowo45@gmail.com>
2022-04-08 18:18:55 +0300

    Добавьте файлы проекта.

---
+++ ExFuncs/From1To5.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExFuncs
+{
+    public class From1To5
+    {//Ex1, Ex2 - Funcs for exs
+        public static double Ex1FindY(double x)
+        {
+            return Math.Pow(Math.Sin(x), 2) + Math.Exp(Math.Pow(Math.Abs(Math.Pow(x, 2) + 1), 5)) - Math.Log(Math.Pow(x, 2) + 1);
+        }
+        public static double Ex2FindY(double x)
+        {
+            return Math.Sin(Math.Pow(x, 2)) / (Math.Cos(Math.Pow(x, 2)) + 5) + Math.Pow(Math.Log(x - 2) / (Math.Sin(Math.Pow(x, 2)) + Math.Pow(x, 2) + 1), 1 / 3);
+        }/// <summary>
+        /// Находит сумму X со степенями x^2^n всевозможных значений, где степень n - maxval for 1 to n
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="n"></param>
+        /// <returns>Сумма S</returns>
+        public static double Ex3FindS(double x, int n)
+        {
+            if (n <= 0) return 0;
+            double sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += Math.Pow(x, Math.Pow(2, i));
+            }
+            return sum;
+        }/// <summary>
+        /// Создает массив, значения которого ai+j(i)
+        /// </summary>
+        /// <param name="masA"></param>
+        /// <returns>masB - созданный массив значений</returns>
+        public static int[] Ex4CreateMas(int[] masA)
+        {
+            if (masA == null) return null;
+            int[] masB = new int[masA.Length];            
+            for (int i = 0; i < masA.Length; i++)
+            {   
+                masB[i] = masA[i] + i;                
+            }            
+            return masB;
+        }/// <summary>
+        /// Происходит заполнение матрицы относительно установленного значения, которое является размером
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns>matrix - полученный двумерный массив, представляющий собой матрицу</returns>
+        public static int[,] GetMatrix(int n)
+        {
+            int[,] matrix = new int[n, n];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                matrix[i, i] = i * (i + 1);
+            }
+            return matrix;
+        }
+    }
+}
---
+++ FuncPW3Lab/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ExFuncs;
+
+namespace FuncPW3Lab
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Ex1FindY_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Ex1Y.Text = From1To5.Ex1FindY(Convert.ToDouble(Ex1X.Text)).ToString();
+            }
+            catch
+            {
+                BadValMessage();
+                Ex1X.Focus();                
+            }
+        }
+        private void BadValMessage()
+        {
+            MessageBox.Show("Введено некорректно значение!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void Ex2FindY_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Ex2Y.Text = From1To5.Ex1FindY(Convert.ToDouble(Ex2X.Text)).ToString();
+            }
+            catch
+            {
+                BadValMessage();
+                Ex2X.Focus();
+            }
+        }
+
+        private void Ex3FindS_Click(object sender, RoutedEventArgs e)
+        {
+            int n;
+            double x;
+            try
+            {
+                x = Convert.ToDouble(Ex3X.Text);
+            }
+            catch
+            {
+                BadValMessage();
+                Ex3X.Focus();
+                return;
+            }
+            try
+            {
+                n = Convert.ToInt32(Ex3N.Text);
+            }
+            catch
+            {
+                BadValMessage();
+                Ex3N.Focus();
+                return;
+            }
+            Ex3S.Text = From1To5.Ex3FindS(x, n).ToString();
+        }
+    }
+}
